5a5b65a0033a8840b21ecde1af36e348f030c1b0
Alan <irwing.ruiz369@gmail.com>
2019-12-17 19:13:31 -0600

    s

---
+++ MonitoreUniversal.Entidades/MunicipioDelegacion.cs
@@ -0,0 +1,9 @@
+﻿namespace MonitoreUniversal.Entidades
+{
+    public class MunicipioDelegacion
+    {
+        public int idMunicipioDelegacion { set; get; }
+        public string descripcion { set; get; }
+        public bool estatus { set; get; }
+    }
+}
